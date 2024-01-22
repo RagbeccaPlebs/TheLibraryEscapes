31f153bf21ccf2aa8a0cfcd029aff0d541652ef3
Rebecca <60572595+RagbeccaPlebs@users.noreply.github.com>
2024-01-22 21:25:48 +0100

    Update

--- LocationPushInteractable.h
+++ LocationPushInteractable.h
@@ -8,7 +8,7 @@ protected:
 	sf::RectangleShape m_FinalPosition;
 public:
 	//Constructor
-	LocationPushInteractable(const int id, const std::string& textureFileLocation, sf::Vector2f position, sf::Vector2f finalPosition, const std::string& fileOfOrigin);
+	LocationPushInteractable(int id, const std::string& textureFileLocation, sf::Vector2f position, sf::Vector2f finalPosition, const std::string& fileOfOrigin);
 
 	//Interaction with interactable
 	std::pair<std::string, sf::Vector2f> Interact() override;
